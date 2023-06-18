f5cc7a5add33ddeec70a3d03b2187bfd83457431
mmmgn2022 <marceline.megan@gmail.com>
2023-06-19 02:33:20 +0700

    bootstrap styling plugin

---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <!-- <h2>NavBar</h2> -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
-createApp(App).use(router).mount('#app')
+
+const app = createApp(App);
+
+// Use router
+app.use(router);
+app.mount('#app');
